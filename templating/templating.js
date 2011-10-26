36abbf7b1ee10003639943e0a6558ecb02ab4774
Keith Donald <keith.donald@springsource.com>
2011-10-26 12:51:05 -0400

    templating example

--- templating/templating.js
+++ templating/templating.js
@@ -1,3 +1,13 @@
-define(["jquery", "handlebars"], function($, handlebars) {
+define(["jquery","handlebars"], function($, handlebars) {
 
+  var template = handlebars.compile($("#template").html());
+  
+  $("#templatingButton").on("click", function() {
+    var user = {
+      name: "Keith Donald",
+      link: "http://twitter.com/kdonald"
+    };
+    $("#templatingResults").html(template(user));
+  });
+  
 });
