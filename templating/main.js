d23dff09e86b6ac867188b9bb0f18be8ebbd37ba
Keith Donald <keith.donald@springsource.com>
2011-10-26 09:50:57 -0400

    templating stub

---
+++ templating/main.js
@@ -0,0 +1,9 @@
+require.config({
+  paths: {
+    jquery: "../lib/jquery-1.7b2/jquery",
+    handlebars: "../lib/handlebars-1.0.0.beta3/handlebars"    
+  }
+});
+require(["templating"], function() {
+  
+});
---
+++ templating/templating.js
@@ -0,0 +1,3 @@
+define(["jquery", "handlebars"], function($, handlebars) {
+
+});
